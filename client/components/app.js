e64069b2d550b2e9d0f67c85d2c881ac2b34246f
Elliot Cheung <elliotccheung@gmail.com>
2017-08-31 13:56:26 -0500

    Wrote app component

---
+++ client/components/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div id="app">
+        Template text, put your render code here!
+      </div>)
+    )
+  }
+}
